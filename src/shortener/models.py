291faa2f767214e9c74749af4ed49d5885978706
marlonxteban <marlonxteban@gmail.com>
2017-02-20 22:24:47 -0500

    siguiendo modelos

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+from .utils import code_generator
+
 class ShortURL(models.Model):
 	url = models.CharField(max_length=220, )
-	shortcode = models.CharField(max_length=15, unique=True)
+	shortcode = models.CharField(max_length=15, unique=True, blank=True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	#empty_dayetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+def save(self, *args, **kwargs):
+	#print('sobreescritura de save')
+	if self.shortcode is None or self.shortcode =="":
+		self.shortcode = code_generator()
+	super(ShortURL, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return str(self.url)
