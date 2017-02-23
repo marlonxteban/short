de83582fd31253a309d437cca20de46eedc7cbb8
marlonxteban <marlonxteban@gmail.com>
2017-02-23 00:17:45 -0500

    managers and commands

---
+++ src/shortener/management/commands/refreshcodes.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from shortener.models import ShortURL
+
+
+class Command(BaseCommand):
+    help = 'Refreshes all shortcodes'
+
+    def add_arguments(self, parser):
+        parser.add_argument('items', type=int)
+
+    def handle(self, *args, **options):
+        return ShortURL.objects.refresh_shortcodes(items=options['items'])
+        
--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,19 +2,40 @@ from django.db import models
 
 # Create your models here.
 
-from .utils import code_generator
+from .utils import code_generator, create_shortcode
+
+class ShortURLManager(models.Manager):
+	"""docstring for ShortURLManager"""
+	def all(self, *args, **kwargs):
+		qs_main = super(ShortURLManager, self).all(*args, **kwargs)
+		qs = qs_main.filter(active=True)
+		return qs
+
+def refresh_shortcodes(self):
+	qs = ShortURL.objects.filter(id__gte=1)
+	if items is not None and instance(items, int):
+		qs = qs.order_by('id')[:items]
+	new_codes = 0
+	for q in qs:
+		q.shortcode = create_shortcode(q)
+		print(q.shortcode)
+		q.save
+		new_codes += 1
+	return "New codes made: {i}".format(i=new_codes)
 
 class ShortURL(models.Model):
 	url = models.CharField(max_length=220, )
 	shortcode = models.CharField(max_length=15, unique=True, blank=True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default=True)
 	#empty_dayetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+	objects = ShortURLManager() #se usa objects.all() para invocar al metodo del manager
 
 def save(self, *args, **kwargs):
 	#print('sobreescritura de save')
 	if self.shortcode is None or self.shortcode =="":
-		self.shortcode = code_generator()
+		self.shortcode = create_shortcode(self)
 	super(ShortURL, self).save(*args, **kwargs)
 
 	def __str__(self):
